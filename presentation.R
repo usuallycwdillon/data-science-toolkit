a134f138687b972e29f6c595354ddcd32591f040
usuallycwdillon <cwdillon@gmail.com>
2014-04-24 21:28:39 -0400

    Thursday update

--- presentation.R
+++ presentation.R
@@ -1,7 +1,11 @@
 library("slidify", lib.loc="/usr/lib64/R/library")
 library("slidifyLibraries", lib.loc="/usr/lib64/R/library")
 library("knitr", lib.loc="/usr/lib64/R/library")
-library("igraph", lib.loc="/usr/lib64/R/library")
+
+# I'll see if I can get the plotly library installed so that any 
+# plots we throw in here can have D3 eye-candy goodness. Otherwise,
+# ggplot2 is the best bet for excellent visualizations
+
 # author("presentation") #running this again erases all our hard work. 
 # Don't do it.
 
